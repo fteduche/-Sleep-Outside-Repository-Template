96f4d1559607653eb1553c595783dbc0cc024835
Fred Frank U <fteduche@gmail.com>
2025-09-23 16:09:03 +0100

    another week

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,11 @@
+import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-// create an instance of ProductData
+loadHeaderFooter();
+
 const dataSource = new ProductData("tents");
-
-// find the list element in the HTML
 const element = document.querySelector(".product-list");
+const productList = new ProductList("Tents", dataSource, element);
 
-// create an instance of ProductList
-const productList = new ProductList("tents", dataSource, listElement);
-
-// call the init method to load and render the list
 productList.init();
